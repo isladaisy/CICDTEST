6315e9b2403c6db3b8b8cc76ac32d97f069d5a56
isladaisy <isladaisyf@gmail.com>
2023-01-19 21:32:19 +0900

    내용, 저자 삭제 후 실험

--- src/main/java/com/example/hanghaeboard/dto/BoardRequestDto.java
+++ src/main/java/com/example/hanghaeboard/dto/BoardRequestDto.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public class BoardRequestDto {
 
     private String title;
-    private String contents;
-    private String author;
+//    private String contents;
+//    private String author;
     private String password;
 
 }
